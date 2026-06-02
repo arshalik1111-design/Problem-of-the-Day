99ec8a3cef31f18e1ace34197d2d8472e27c9b26
Arsh Ali Khan <arshalik1111@gmail.com>
2026-06-02 22:00:22 +0530

    Leetcode 3633 POTD Solution

--- LeetCode/potd.cpp
+++ LeetCode/potd.cpp
@@ -4,6 +4,7 @@ using namespace std;
 class Solution
 {
 public:
+    // 1 June 2026
     // 2144. Minimum Cost of Buying Candies With Discount
     int minimumCost(vector<int> &nums)
     {
@@ -27,17 +28,65 @@ public:
         }
         return sum;
     }
+
+    // 3633. Earliest Finish Time for Land and Water Rides I
+
+    int checkOrder(vector<int> &landStartTime, vector<int> &landDuration, vector<int> &waterStartTime, vector<int> &waterDuration)
+    {
+        int minFirstEndTime = INT_MAX;
+        for (int i = 0; i < landStartTime.size(); i++)
+        {
+            minFirstEndTime = min(minFirstEndTime, landStartTime[i] + landDuration[i]);
+        }
+        int totalEndTime = INT_MAX;
+        for (int i = 0; i < waterDuration.size(); i++)
+        {
+            int startTimeOfSecond = max(minFirstEndTime, waterStartTime[i]);
+            int secondRideTime = startTimeOfSecond + waterDuration[i];
+            totalEndTime = min(totalEndTime, secondRideTime);
+        }
+        return totalEndTime;
+    }
+
+    int earliestFinishTime(vector<int> &landStartTime, vector<int> &landDuration, vector<int> &waterStartTime, vector<int> &waterDuration)
+    {
+        int landTime = checkOrder(landStartTime, landDuration, waterStartTime, waterDuration);
+        int waterTime = checkOrder(waterStartTime, waterDuration, landStartTime, landDuration);
+
+        return min(landTime, waterTime);
+    }
 };
 int main()
 {
     int n;
     cin >> n;
-    vector<int> nums(n);
+    int m;
+    cin >> m;
+    int p;
+    cin >> p;
+    int q;
+    cin >> q;
+    vector<int> nums1(n);
     for (int i = 0; i < n; i++)
     {
-        cin >> nums[i];
+        cin >> nums1[i];
+    }
+    vector<int> nums2(m);
+    for (int i = 0; i < m; i++)
+    {
+        cin >> nums2[i];
+    }
+    vector<int> nums3(p);
+    for (int i = 0; i < p; i++)
+    {
+        cin >> nums3[i];
+    }
+    vector<int> nums4(q);
+    for (int i = 0; i < q; i++)
+    {
+        cin >> nums4[i];
     }
     Solution solution;
-    int ans = solution.minimumCost(nums);
+    int ans = solution.earliestFinishTime(nums1, nums2, nums3, nums4);
     cout << ans;
 }
