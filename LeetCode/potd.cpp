f993b81f14ea24ef15e467666420449cfe127b30
Arsh Ali Khan <arshalik1111@gmail.com>
2026-06-04 23:58:43 +0530

    Leetcode 3751 POTD Solution

--- LeetCode/potd.cpp
+++ LeetCode/potd.cpp
@@ -55,6 +55,34 @@ public:
 
         return min(landTime, waterTime);
     }
+    // 3751. Total Waviness of Numbers in Range I
+
+    int findWaviness(int num)
+    {
+        string s = to_string(num);
+        if (s.length() < 3)
+            return 0;
+        int count = 0;
+
+        for (int i = 1; i < s.length() - 1; i++)
+        {
+            if ((s[i] > s[i - 1] && s[i] > s[i + 1]))
+                count++;
+            if ((s[i] < s[i + 1] && s[i] < s[i - 1]))
+                count++;
+        }
+        return count;
+    }
+    int totalWaviness(int num1, int num2)
+    {
+        int score = 0;
+
+        for (int num = num1; num <= num2; num++)
+        {
+            score += findWaviness(num);
+        }
+        return score;
+    }
 };
 int main()
 {
@@ -62,31 +90,31 @@ int main()
     cin >> n;
     int m;
     cin >> m;
-    int p;
-    cin >> p;
-    int q;
-    cin >> q;
-    vector<int> nums1(n);
-    for (int i = 0; i < n; i++)
-    {
-        cin >> nums1[i];
-    }
-    vector<int> nums2(m);
-    for (int i = 0; i < m; i++)
-    {
-        cin >> nums2[i];
-    }
-    vector<int> nums3(p);
-    for (int i = 0; i < p; i++)
-    {
-        cin >> nums3[i];
-    }
-    vector<int> nums4(q);
-    for (int i = 0; i < q; i++)
-    {
-        cin >> nums4[i];
-    }
+    // int p;
+    // cin >> p;
+    // int q;
+    // cin >> q;
+    // vector<int> nums1(n);
+    // for (int i = 0; i < n; i++)
+    // {
+    //     cin >> nums1[i];
+    // }
+    // vector<int> nums2(m);
+    // for (int i = 0; i < m; i++)
+    // {
+    //     cin >> nums2[i];
+    // }
+    // vector<int> nums3(p);
+    // for (int i = 0; i < p; i++)
+    // {
+    //     cin >> nums3[i];
+    // }
+    // vector<int> nums4(q);
+    // for (int i = 0; i < q; i++)
+    // {
+    //     cin >> nums4[i];
+    // }
     Solution solution;
-    int ans = solution.earliestFinishTime(nums1, nums2, nums3, nums4);
+    int ans = solution.totalWaviness(n, m);
     cout << ans;
 }
